f8e65d84c07bcd5c356a327679a1d5a21b6dd433
vinhnguyenx <vinh.nguyen09102003@gmail.com>
2025-10-02 15:47:55 -0700

    remove grid for pieces

--- piece.py
+++ piece.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygame
 from grid import Grid
 from config import *
 
@@ -11,82 +12,90 @@ class Piece(Grid):
     def random_piece(self):
         """Generate a random piece shape inside a 5x5 grid."""
         shapes = [
-            # 3x3 solid block
             [[1, 1, 1],
              [1, 1, 1],
              [1, 1, 1]],
 
-            # 2x2 square
             [[1, 1, 1],
              [1, 0, 0],
              [1, 0, 0]],
-            
+
             [[1, 0, 0],
              [1, 0, 0],
              [1, 1, 1]],
-            
+
             [[1, 0, 0],
              [0, 1, 0],
              [0, 0, 1]],
-            
+
             [[0, 0, 1],
              [0, 1, 0],
              [1, 0, 0]],
-            
+
             [[0, 1, 1],
              [1, 1, 0]],
-            
+
             [[1, 0, 0],
              [1, 1, 1]],
-            
+
             [[0, 1, 0],
              [1, 1, 1]],
-            
+
             [[1, 1]],
-            
+
             [[1, 1, 1]],
 
-            # Horizontal 4-line
             [[1, 1, 1, 1]],
-            
+
             [[1, 1, 1, 1, 1]],
-            
+
             [[1, 1],
              [0, 1],
              [0, 1]],
-            
+
             [[0, 1],
              [1, 1],
              [1, 0]],
-            
-             [[0, 1],
-              [1, 0]],
-             
-             [[0, 1],
+
+            [[0, 1],
+             [1, 0]],
+
+            [[0, 1],
              [1, 1],
              [0, 1]],
-            
+
             [[1],
              [1]],
 
-            # Vertical 4-line
             [[1],
              [1],
              [1],
              [1]],
 
-            # Vertical 5-line
             [[1],
              [1],
              [1],
              [1],
              [1]],
 
-            # L-shape
             [[1, 1],
              [0, 1]],
         ]
 
         shape = np.array(shapes[np.random.randint(len(shapes))], dtype=int)
-        self.grid.fill(0)  # reset
+        self.grid.fill(0)
         self.grid[:shape.shape[0], :shape.shape[1]] = shape
+
+    def draw(self, surface, offset_x=0, offset_y=0):
+        """Draw only the shape (1’s), no grid outline."""
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.grid[r, c] == 1:
+                    x = offset_x + c * self.cell_size
+                    y = offset_y + r * self.cell_size
+                    rect = pygame.Rect(x, y, self.cell_size, self.cell_size)
+
+                    # Filled green block with outline
+                    inner_rect = rect.inflate(-6, -6)  
+                    pygame.draw.rect(surface, (0, 200, 0), inner_rect)
+                    pygame.draw.rect(surface, (0, 255, 0), rect, 2)
